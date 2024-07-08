7bd16aed11863061af2f12c7c73d1422ba09a545
abdulsamad <alogbaabdulsamad@gmail.com>
2024-07-07 23:12:49 -0500

    first commit

---
+++ index2.js
@@ -0,0 +1,30 @@
+var frontpagediv =  document.getElementById("frontpagedivid");
+var gamediv = document.getElementById("grid")
+var backarrow = document.getElementById("backarrowid")
+var isonsomethingelse = false
+var updatesdiv =  document.getElementById("updatesdivid");
+
+
+
+
+function sussy(){
+  frontpagediv.style.transform = 'translateY(-150%)';
+  gamediv.style.transform = 'translateY(-100%)';
+  backarrow.style.opacity = 1;
+}
+
+backarrow.onclick = function(){
+  if (backarrow.style.opacity == 1){
+    gamediv.style.transform = 'translateY(600px)';
+    frontpagediv.style.transform = 'translateY(0%)';
+    updatesdiv.style.transform = 'translateY(600px)';
+    backarrow.style.opacity = 0;
+  }
+
+}
+
+function susssy(){
+  frontpagediv.style.transform = 'translateY(-150%)';
+  updatesdiv.style.transform = 'translateY(-220%)';
+  backarrow.style.opacity = 1;
+}
---
+++ index3.js
@@ -0,0 +1,38 @@
+function handleGameButtonClick(button, src){
+  button.onclick = function(){
+    window.open(src, '_blank')
+  }
+}
+
+handleGameButtonClick(buttonminute, "/gameslist/10minutestilldawn/index.html");
+handleGameButtonClick(button2048,"/gameslist/2048/index.html");
+handleGameButtonClick(buttonworld,"/gameslist/asmallworldcup/index.html");
+handleGameButtonClick(buttontanks,"/gameslist/awsometanks2/index.html");
+handleGameButtonClick(buttonbros,"/gameslist/basketbros/index.html");
+handleGameButtonClick(buttonbasket, "/gameslist/basketrandom/index.html");
+handleGameButtonClick(buttonboxing, "/gameslist/boxingrandom/index.html");
+handleGameButtonClick(buttoncookie,  "/gameslist/cookieclicker/index.html");
+handleGameButtonClick(buttoncube, "/gameslist/cubefield/index.html");
+handleGameButtonClick(buttondeath, "/gameslist/deathrun3d/index.html");
+handleGameButtonClick(buttoncrossy,  "/gameslist/crossyroads/index.html");
+handleGameButtonClick(buttondraw, "/gameslist/drawthehill/index.html");
+handleGameButtonClick(buttondrift,  "/gameslist/driftboss/index.html");
+handleGameButtonClick(buttondrive, "/gameslist/drivemad/index.html");
+handleGameButtonClick(buttonfluid,  "/gameslist/fluidsim/index.html");
+handleGameButtonClick(buttongun2,"/gameslist/gunmayhem2/index.html");
+handleGameButtonClick(buttongunredux,"/gameslist/gunmayhemredux/index.html");
+handleGameButtonClick(buttonjoyride, "/gameslist/jetpackjoyride/index.html");
+handleGameButtonClick(buttonmonkey,"/gameslist/monkeymart/index.html");
+handleGameButtonClick(buttonparkour, "/gameslist/noobsteveparkour/index.html");
+handleGameButtonClick(buttonpaper,"/gameslist/paperio/index.html");
+handleGameButtonClick(buttonpapery, "/gameslist/paperyplanes/index.html");
+handleGameButtonClick(buttonpush,  "/gameslist/pushthesquare/index.html");
+handleGameButtonClick(buttonradius,  "/gameslist/radiusraid/index.html");
+handleGameButtonClick(buttonslope, "/gameslist/slope/index.html");
+handleGameButtonClick(buttonsoccer,  "/gameslist/soccerrandom/index.html");
+handleGameButtonClick(buttonvex, "/gameslist/vex6/index.html");
+handleGameButtonClick(buttonsubway, "/gameslist/subwaysurfers/index.html");
+handleGameButtonClick(buttonvolley,  "/gameslist/volleyrandom/index.html");
+handleGameButtonClick(buttonwordle,  "/gameslist/wordle/index.html");
+
+
